cfde158115228538a74595f4cb88dda071a73770
mrrishi <mrrishi373@gmail.com>
2025-10-21 04:34:43 +0530

    fix: azs

--- terraform/aws/modules/workload-business-cluster/main.tf
+++ terraform/aws/modules/workload-business-cluster/main.tf
@@ -3,7 +3,7 @@ data "aws_availability_zones" "available" {}
 locals {
   cluster_version = "1.31"
   vpc_cidr        = "10.0.0.0/16"
-  azs             = slice(data.aws_availability_zones.available.names, 0, 3)
+  azs             = slice(data.aws_availability_zones.available.names, 0, min(length(data.aws_availability_zones.available.names), 3))
   tags = {
     kubefirst = "true"
   }
