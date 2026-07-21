bd36e8e18af0e4e7ae411b135b69b20b56086d0a
Rishi <117034340+jokestax@users.noreply.github.com>
2026-07-22 01:56:18 +0530

    Update main.tf

--- terraform/civo/modules/project-cluster/main.tf
+++ terraform/civo/modules/project-cluster/main.tf
@@ -137,29 +137,6 @@ resource "kubernetes_secret_v1" "argocd_manager" {
   depends_on = [kubernetes_service_account_v1.argocd_manager]
 }
 
-# ── external-dns
-
-resource "kubernetes_namespace_v1" "external_dns" {
-  metadata {
-    name = "external-dns"
-  }
-}
-
-data "vault_generic_secret" "external_dns" {
-  path = "secret/external-dns"
-}
-
-resource "kubernetes_secret_v1" "external_dns" {
-  metadata {
-    name      = "external-dns-secrets"
-    namespace = kubernetes_namespace_v1.external_dns.metadata.0.name
-  }
-  data = {
-    token = data.vault_generic_secret.external_dns.data["token"]
-  }
-  type = "Opaque"
-}
-
 # ──────────────────────────────────────────────
 # Crossplane Secrets
 # ──────────────────────────────────────────────
